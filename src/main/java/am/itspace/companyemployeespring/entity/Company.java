3d22267ff368ad7d48ab85492e96945191ed62ee
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-26 17:44:43 +0400

    Company

--- src/main/java/am/itspace/companyemployeespring/entity/Company.java
+++ src/main/java/am/itspace/companyemployeespring/entity/Company.java
@@ -1,2 +1,28 @@
-package am.itspace.companyemployeespring.entity;public class Company {
+package am.itspace.companyemployeespring.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private int size;
+    private String address;
+
 }
