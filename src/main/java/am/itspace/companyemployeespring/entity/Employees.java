b891f1094a01953f56bee62c6211dff12b391b9e
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-26 17:43:24 +0400

    Employees

--- src/main/java/am/itspace/companyemployeespring/entity/Employees.java
+++ src/main/java/am/itspace/companyemployeespring/entity/Employees.java
@@ -1,2 +1,30 @@
-package am.itspace.companyemployeespring.entity;public class Employees {
+package am.itspace.companyemployeespring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employees {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phoneNumber;
+    private double salary;
+    private String position;
+    @ManyToOne
+    private Company company;
+    private String profilePic;
+
 }
