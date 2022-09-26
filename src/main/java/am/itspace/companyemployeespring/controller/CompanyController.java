ac39a7f198cd64718ee2fd2663e20d6552239db1
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-26 17:44:57 +0400

    CompanyController

--- src/main/java/am/itspace/companyemployeespring/controller/CompanyController.java
+++ src/main/java/am/itspace/companyemployeespring/controller/CompanyController.java
@@ -1,2 +1,47 @@
-package am.itspace.companyemployeespring.controller;public class CompanyController {
+package am.itspace.companyemployeespring.controller;
+
+import am.itspace.companyemployeespring.entity.Company;
+import am.itspace.companyemployeespring.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class CompanyController {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+
+    @GetMapping("/company")
+    public String company(ModelMap modelMap) {
+        List<Company> companies = companyRepository.findAll();
+        modelMap.addAttribute("companies", companies);
+        return "company";
+    }
+
+
+    @GetMapping("/company/add")
+    public String addCompanyPage() {
+        return "addCompany";
+    }
+
+
+    @PostMapping("/company/add")
+    public String addCompany(@ModelAttribute Company company) {
+        companyRepository.save(company);
+        return "redirect:/company";
+    }
+
+    @GetMapping("/company/delete")
+    public String deleteCompany(@RequestParam("id") int id) {
+        companyRepository.deleteById(id);
+        return "redirect:/company";
+    }
 }
