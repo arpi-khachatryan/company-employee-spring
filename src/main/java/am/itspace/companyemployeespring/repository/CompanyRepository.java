22c669da90db00ab4f309c1bbbdc129931206103
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-26 17:44:32 +0400

    CompanyRepository

--- src/main/java/am/itspace/companyemployeespring/repository/CompanyRepository.java
+++ src/main/java/am/itspace/companyemployeespring/repository/CompanyRepository.java
@@ -1,2 +1,8 @@
-package am.itspace.companyemployeespring.repository;public class CompanyRepository {
+package am.itspace.companyemployeespring.repository;
+
+import am.itspace.companyemployeespring.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
+
 }
