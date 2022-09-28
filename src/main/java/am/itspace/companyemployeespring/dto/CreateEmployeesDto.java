cdaceabfc74aaf2df4fa43070be53f37d2e0ff6f
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-28 11:24:07 +0400

    CreateEmployeesDto

--- src/main/java/am/itspace/companyemployeespring/dto/CreateEmployeesDto.java
+++ src/main/java/am/itspace/companyemployeespring/dto/CreateEmployeesDto.java
@@ -1,13 +1,10 @@
 package am.itspace.companyemployeespring.dto;
 
-
-import am.itspace.companyemployeespring.entity.Company;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.ManyToOne;
 
 @Data
 @Builder
@@ -21,8 +18,6 @@ public class CreateEmployeesDto {
     private String phoneNumber;
     private double salary;
     private String position;
-    @ManyToOne
     private Integer companyId;
     private String profilePic;
-
 }
