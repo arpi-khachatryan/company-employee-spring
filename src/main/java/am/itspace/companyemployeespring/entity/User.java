50f7eefbb57d534a6bef82784270143d3d3cabfe
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-30 17:57:59 +0400

    User

---
+++ src/main/java/am/itspace/companyemployeespring/entity/User.java
@@ -0,0 +1,29 @@
+package am.itspace.companyemployeespring.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "user")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+    private String picUrl;
+    private String password;
+}
