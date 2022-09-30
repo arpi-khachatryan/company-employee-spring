eaf61d6a0004bdca61456ec41770fbac6cbb288e
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-09-30 17:57:00 +0400

    WebSecurityConfig

---
+++ src/main/java/am/itspace/companyemployeespring/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package am.itspace.companyemployeespring.config;
+
+import am.itspace.companyemployeespring.entity.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .formLogin()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/company").permitAll()
+                .antMatchers("/company/add").hasAuthority(Role.ADMIN.name())
+                .antMatchers("/employees").authenticated()
+                .antMatchers("/users").authenticated()
+                .antMatchers("/users/add").permitAll()
+                .anyRequest().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+    }
+}
