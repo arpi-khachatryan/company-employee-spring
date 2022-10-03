ac318c2ba683c7c7d91d01af5bcc45e6419fa029
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-03 16:20:48 +0400

    MainController

--- src/main/java/am/itspace/companyemployeespring/controller/MainController.java
+++ src/main/java/am/itspace/companyemployeespring/controller/MainController.java
@@ -1,7 +1,13 @@
 package am.itspace.companyemployeespring.controller;
 
+import am.itspace.companyemployeespring.entity.Role;
+import am.itspace.companyemployeespring.entity.User;
+import am.itspace.companyemployeespring.security.CurrentUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -9,5 +15,31 @@ public class MainController {
     @GetMapping("/")
     public String mainPage() {
         return "index";
+    }
+
+    @GetMapping("loginPage")
+    public String loginPage(@RequestParam(value = "error", required = false) String error, ModelMap modelMap) {
+        if (error != null && error.equals("true")) {
+            modelMap.addAttribute("error", "true");
+        }
+        return "loginPage";
+    }
+
+    @GetMapping("/loginSuccess")
+    public String loginSuccess(@AuthenticationPrincipal CurrentUser currentUser) {
+        if (currentUser != null) {
+            User user = currentUser.getUser();
+            if (user.getRole() == Role.ADMIN) {
+                return "redirect:/admin";
+            } else if (user.getRole() == Role.USER) {
+                return "redirect:/user";
+            }
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("accessDenied")
+    public String accessDenied() {
+        return "accessDenied";
     }
 }
